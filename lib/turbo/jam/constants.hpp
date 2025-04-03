d5a2ec42008c0760a53a57479d05d76a8b7ad294
Alex Sierkov <alex.sierkov@gmail.com>
2025-04-04 02:56:03 +0300

    provide the respective JAM paper symbols for some of the constants

--- lib/turbo/jam/constants.hpp
+++ lib/turbo/jam/constants.hpp
@@ -7,21 +7,34 @@
 #include <cstddef>
 
 namespace turbo::jam {
+
+    // JAM paper: I.4.4
+
     struct config_prod {
+        // JAM I.4.4: E
         static constexpr size_t epoch_length = 600;
+
         static constexpr size_t core_count = 341;
+        // JAM I.4.4: V
         static constexpr size_t validator_count = core_count * 3;
         static constexpr size_t validator_super_majority = validator_count * 2 / 3 + 1;
         static constexpr size_t avail_bitfield_bytes = (core_count + 7) / 8;
+        // JAM I.4.4: K
         static constexpr size_t max_tickets_per_block = 16;
+        // JAM I.4.4: N
         static constexpr size_t tickets_per_validator = 2;
+        // JAM I.4.4: H
         static constexpr size_t max_blocks_history = 8;
+        // JAM I.4.4: O
         static constexpr size_t auth_pool_max_size = 8;
+        // JAM I.4.4: Q
         static constexpr size_t auth_queue_size = 80;
+        // JAM I.4.4: R
         static constexpr size_t core_assignment_rotation_period = 10;
         static constexpr size_t ticket_attempts = 2;
         static constexpr size_t max_blob_size = 48 << 10;
-        static constexpr size_t reported_work_timeout = 5; // JAM Paper: U
+        // JAM I.4.4: U
+        static constexpr size_t reported_work_timeout = 5;
     };
 
     struct config_tiny: config_prod {
