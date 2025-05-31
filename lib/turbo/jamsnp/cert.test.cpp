94220a9f5c00a5919931fdb3b40c76d34bbe50e1
Alex Sierkov <alex.sierkov@gmail.com>
2025-05-31 02:42:39 +0200

    add an addition test vector for cert_name generation

--- lib/turbo/jamsnp/cert.test.cpp
+++ lib/turbo/jamsnp/cert.test.cpp
@@ -20,8 +20,10 @@ suite turbo_jamsnp_cert_suite = [] {
         expect_equal("7ara", cert_name_base32(uint8_vector::from_hex("1F44")));
         expect_equal("vkva", cert_name_base32(uint8_vector::from_hex("5555")));
         expect_equal("kvkb", cert_name_base32(uint8_vector::from_hex("aaaa")));
+
         expect_equal("eaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", cert_name_from_vk(vkey_t::from_hex("0000000000000000000000000000000000000000000000000000000000000000")));
         expect_equal("e777777777777777777777777777777777777777777777777777b", cert_name_from_vk(vkey_t::from_hex("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF")));
+        expect_equal("e3r2oc62zwfj3crnuifuvsxvbtlzetk4o5qyhetkhagsc2fgl2oka", cert_name_from_vk(vkey_t::from_hex("3b6a27bcceb6a42d62a3a8d02a6f0d73653215771de243a63ac048a18b59da29")));
         expect_equal("eaieeszqivszohegtk5oz4o357hacbrmgekvmw3brzukxlgx3o77b", cert_name_from_vk(vkey_t::from_hex("00112233445566778899AABBCCDDEEFF00112233445566778899AABBCCDDEEFF")));
     };
 };
