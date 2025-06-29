652a481586a835861a49d2ac560a52c31cdda673
Alex Sierkov <alex.sierkov@gmail.com>
2025-06-29 13:47:24 +0200

    replace common with a submodule

--- lib/turbo/common/benchmark.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-/* Copyright (c) 2022-2023 Alex Sierkov (alex dot sierkov at gmail dot com)
- * Copyright (c) 2024-2025 R2 Rationality OÜ (info at r2rationality dot com) */
-
-#include <nanobench.h>
-#include "test.hpp"
--- lib/turbo/common/bytes.cpp
+++
@@ -1,8 +0,0 @@
-#include "bytes.hpp"
-
-namespace turbo {
-    void secure_clear(std::span<uint8_t> store)
-    {
-        std::fill_n<volatile uint8_t *>(store.data(), store.size(), 0);
-    }
-}
