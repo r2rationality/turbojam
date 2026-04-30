016c9f6ae54d7efdf2c0808b7181f69270b6c9e4
Alex Sierkov <alex.sierkov@gmail.com>
2026-04-30 15:35:55 +0300

    improve conformance with JAM_FUZZ environment

--- src/tjam.cpp
+++ src/tjam.cpp
@@ -1,14 +1,19 @@
 #ifdef MI_OVERRIDE
 #   include <mimalloc-new-delete.h>
-#   include <iostream>
 #endif
+#include <iostream>
 #include <turbo/common/cli.hpp>
 
 int main(const int argc, const char **argv)
 {
 #ifdef MI_OVERRIDE
-    std::cerr << "DT_INIT: mimalloc " << mi_version() << '\n';
+    std::cerr << "INIT: mimalloc " << mi_version() << '\n';
 #endif
     using namespace turbo;
+    if (const auto *data_path = std::getenv("JAM_FUZZ_DATA_PATH"); std::getenv("JAM_FUZZ") && data_path) {
+        const auto log_path = fmt::format("{}/tjam-fuzz.log", data_path);
+        std::cerr << fmt::format("INIT: JAM_FUZZ log path override: {}\n", log_path);
+        logger::init_log_path(log_path);
+    }
     return cli::run(argc, argv);
 }
