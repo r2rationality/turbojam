1e323da771bfbe71722236cec641f3ab0f91514c
Alex Sierkov <alex.sierkov@gmail.com>
2026-04-30 12:08:11 +0300

    Make Dockerfile.fuzz compatible with JAM Standard Target Packaging

--- lib/turbo/jam/cli/fuzzer-api.cpp
+++ lib/turbo/jam/cli/fuzzer-api.cpp
@@ -7,6 +7,7 @@
 // https://github.com/davxy/jam-stuff/tree/main/fuzz-proto
 
 #include <turbo/common/cli.hpp>
+#include <turbo/common/scope-exit.hpp>
 #include <turbo/common/variant.hpp>
 #include <turbo/jam/chain.hpp>
 #include <turbo/jam/fuzzer-runner.hpp>
@@ -26,51 +27,50 @@ namespace {
 
     template<typename CFG>
     struct server_t {
-        server_t(std::string chain_id, std::string sock_path):
+        server_t(std::string chain_id, std::string data_path, std::string sock_path):
             _chain_id{std::move(chain_id)},
+            _data_path{std::move(data_path)},
             _sock_path{std::move(sock_path)}
         {
             boost::asio::co_spawn(_ioc, _listen(), boost::asio::detached);
         }
 
-        ~server_t() noexcept
-        {
+        ~server_t() noexcept {
             std::filesystem::remove(_sock_path);
         }
 
-        void run()
-        {
+        void run() {
             boost::asio::signal_set signals{_ioc, SIGINT, SIGTERM};
             signals.async_wait([this](auto, auto){ _ioc.stop(); });
             _ioc.run();
         }
     private:
         std::string _chain_id;
+        std::string _data_path;
         std::string _sock_path;
         boost::asio::io_context _ioc{};
         std::atomic<uint64_t> _next_client_id{};
 
         boost::asio::awaitable<void> _handle_client(stream_protocol::socket conn, const uint64_t client_id)
-        {
-            try {
-                static const peer_info_t my_peer_info{};
-                uint8_vector read_buf{};
-                {
-                    const auto handshake = co_await read_message<CFG>(conn, read_buf);
-                    const peer_info_t &peer_info = variant::get_nice<peer_info_t>(handshake);
-                    co_await write_message<CFG>(conn, message_t<CFG>{my_peer_info});
-                    my_peer_info.compatible_with(peer_info);
-                }
-                file::tmp_directory tmp_dir{fmt::format("turbo-jam-fuzzer-{}", client_id)};
-                local_processor_t<CFG> processor{_chain_id, tmp_dir.path()};
-                for (;;) {
-                    auto msg_in = co_await read_message<CFG>(conn, read_buf);
-                    auto msg_out = co_await processor.process(std::move(msg_in));
-                    co_await write_message(conn, std::move(msg_out));
-                }
-            } catch (const std::exception &ex) {
-                logger::info("client disconnected: {}", ex.what());
+        try {
+            const auto client_data_path = fmt::format("{}/client-{}", _data_path, client_id);
+            scope_exit{[&]{ std::filesystem::remove_all(client_data_path); }};
+            static const peer_info_t my_peer_info{};
+            uint8_vector read_buf{};
+            {
+                const auto handshake = co_await read_message<CFG>(conn, read_buf);
+                const peer_info_t &peer_info = variant::get_nice<peer_info_t>(handshake);
+                co_await write_message<CFG>(conn, message_t<CFG>{my_peer_info});
+                my_peer_info.compatible_with(peer_info);
             }
+            local_processor_t<CFG> processor{_chain_id, client_data_path};
+            for (;;) {
+                auto msg_in = co_await read_message<CFG>(conn, read_buf);
+                auto msg_out = co_await processor.process(std::move(msg_in));
+                co_await write_message(conn, std::move(msg_out));
+            }
+        } catch (const std::exception &ex) {
+            logger::info("client disconnected: {}", ex.what());
         }
 
         boost::asio::awaitable<void> _listen()
@@ -96,35 +96,69 @@ namespace {
 
 namespace turbo::cli::fuzzer_api {
     struct cmd: command {
-        void configure(config &cmd) const override
-        {
+        void configure(config &cmd) const override {
             cmd.name = "fuzzer-api";
-            cmd.desc = "Launch a local fuzzing API listening at <unix-socket-path>";
-            cmd.args.expect({ "<unix-socket-path>" });
-            cmd.opts.try_emplace(
-                "cfg", "parameter set: either tiny or full", "tiny",
-                [](const std::optional<std::string> &v)-> std::optional<std::string> {
-                    if (!(v && (*v == "tiny" || *v == "full"))) {
-                        return "cfg can be either tiny or full";
+            cmd.desc = "Launch a local fuzzing API listening";
+            if (!_jam_fuzz_args()) {
+                cmd.args.expect({ "<unix-socket-path>" });
+                cmd.opts.try_emplace(
+                    "spec", "parameter set: either tiny or full", "tiny",
+                    [](const std::optional<std::string> &v)-> std::optional<std::string> {
+                        if (!(v && (*v == "tiny" || *v == "full"))) {
+                            return "can be either tiny or full";
+                        }
+                        return {};
                     }
-                    return {};
-                }
-            );
+                );
+            }
         }
 
-        void run(const arguments &args, const options &opts) const override
-        {
-            const auto &sock_path = args.at(0);
-            const auto cfg = opts.at("cfg").value();
-            if (cfg == "tiny") {
+        void run(const arguments &args, const options &opts) const override {
+            std::optional<file::tmp_directory> tmp_dir{};
+            auto fuzz_args = _jam_fuzz_args();
+            if (!fuzz_args) {
+                tmp_dir.emplace("turbo-jam-fuzzer-api");
+                fuzz_args.emplace(opts.at("spec").value(), tmp_dir->path(), args.at(0));
+            }
+            if (fuzz_args->spec == "tiny") {
                 logger::info("starting a fuzzer API with tiny config");
-                server_t<config_tiny> srv{"dev", sock_path};
+                server_t<config_tiny> srv{"dev", fuzz_args->data_path, fuzz_args->sock_path};
+                srv.run();
+            } else if (fuzz_args->spec == "full") {
+                logger::info("starting a fuzzer API with full config");
+                server_t<config_prod> srv{"dev", fuzz_args->data_path, fuzz_args->sock_path};
                 srv.run();
             } else {
-                logger::info("starting a fuzzer API with full config");
-                server_t<config_prod> srv{"dev", sock_path};
-                srv.run();
+                throw error(fmt::format("unsupported spec value: {}", fuzz_args->spec));
             }
+        }
+    private:
+        struct jam_fuzz_args_t {
+            std::string spec{};
+            std::string data_path{};
+            std::string sock_path{};
+
+            static std::optional<jam_fuzz_args_t> from_env() {
+                std::optional<jam_fuzz_args_t> res{};
+                if (std::getenv("JAM_FUZZ")) {
+                    const auto *spec = std::getenv("JAM_FUZZ_SPEC");
+                    if (!spec) [[unlikely]]
+                        throw error("JAM_FUZZ_SPEC environment variable must be set when JAM_FUZZ is set");
+                    const auto *data_path = std::getenv("JAM_FUZZ_DATA_PATH");
+                    if (!data_path) [[unlikely]]
+                        throw error("JAM_FUZZ_DATA_PATH environment variable must be set when JAM_FUZZ is set");
+                    const auto *sock_path = std::getenv("JAM_FUZZ_SOCK_PATH");
+                    if (!sock_path) [[unlikely]]
+                        throw error("JAM_FUZZ_SOCK_PATH environment variable must be set when JAM_FUZZ is set");
+                    res.emplace(spec, data_path, sock_path);
+                }
+                return res;
+            }
+        };
+
+        static const std::optional<jam_fuzz_args_t> &_jam_fuzz_args() {
+            static const auto args = jam_fuzz_args_t::from_env();
+            return args;
         }
     };
     static auto instance = command::reg(std::make_shared<cmd>());
